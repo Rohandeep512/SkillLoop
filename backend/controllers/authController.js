0160ef2836354583a372d50aadc85f9a0426c050
Rohan <rohandeep512@gmail.com>
2026-06-19 02:33:46 +0530

    Set token expiration to 1 day

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import User from '../models/User.js'
 
-const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '7d' })
+const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' })
 
 export const register = async (req, res) => {
   try {
